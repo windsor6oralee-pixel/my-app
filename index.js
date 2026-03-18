4c542c94b96c8ab0241c8175a1e65b901a8e13d0
AHMAD <windsor6oralee@gmail.com>
2026-03-18 18:28:45 +0300

    convert to simple API

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-console.log("Hello World"); 
+const http = require("http");
+
+const server = http.createServer((req, res) => {
+    res.write("Hello from your first API 🚀");
+    res.end();
+});
+
+server.listen(3000, () => {
+    console.log("Server running on http://localhost:3000");
+});
